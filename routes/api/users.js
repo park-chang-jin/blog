dfc68f622f12b18b35cb7460bebc8b43be8f1f66
park <ckdwls2525@naver.com>
2019-06-24 19:26:59 +0900

    user registeration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const gravater = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+const userModel = require('../../models/user');
 
 // @route GET api/users/test
 // @desc Tests users route
@@ -16,11 +20,51 @@ router.get('/test', (req, res) => {
 // @access Public
 
 router.post('/register', (req, res) => {
-    res.json({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    });
+    // res.json({
+    //     name: req.body.name,
+    //     email: req.body.email,
+    //     password: req.body.password
+    // });
+    userModel
+    // Database Email confing
+        .findOne({ email: req.body.email })
+        .then(user => {
+            if (user) {
+                return res.status(400).json({
+                    msg: "Email already exists"
+                });
+            } else {
+                // avatar Create
+                const avatar = gravater.url(req.body.email, {
+                    s: '200', //size
+                    r: 'pg', // Rating
+                    d: 'mm' //Default
+                });
+                // Database Schema Create
+                const newUser = new userModel({
+                    name: req.body.name,
+                    email: req.body.email,
+                    avatar,
+                    password: req.body.password
+                });
+                // Password 암호화
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        newUser.password = hash;
+                        // 저장하고 뿌려줌
+                        newUser.save()
+                                .then(user => res.json(user))
+                                .catch(err => res.json(err));
+                    });
+                });
+                
+
+            }
+        })
+        .catch(err => {
+            res.json(err);    
+        });
 });
 
 
