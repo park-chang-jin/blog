1ddba8747b569f44564d0cc30650a0ed0bc8e93b
park <ckdwls2525@naver.com>
2019-06-24 18:58:32 +0900

    add mongoose

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const postRouter = require('./routes/api/post');
 const profileRouter = require('./routes/api/profile');
 const usersRouter = require('./routes/api/users');
 
+const db = require('./config/keys').mongoURI;
+mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true})
+    .then( () => console.log('MongoDB Connected...'))
+    .catch( err => console.log(err));
+
 app.use(morgan('dev'));
+
+
 
 app.use('/api/post', postRouter);
 app.use('/api/profile', profileRouter);
