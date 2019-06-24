edd4c0e6016f1188d94a8181919421ac6cd7adbc
park <ckdwls2525@naver.com>
2019-06-24 18:46:38 +0900

    router

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 
+const postRouter = require('./routes/api/post');
+const profileRouter = require('./routes/api/profile');
+const usersRouter = require('./routes/api/users');
+
+app.use('/api/post', postRouter);
+app.use('/api/profile', profileRouter);
+app.use('/api/users', usersRouter);
+
 const PORT = 3000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
