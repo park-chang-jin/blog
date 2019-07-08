13e83837774bc14776d2c6a8f1f41c036ffa40a7
park <ckdwls2525@naver.com>
2019-07-08 19:31:13 +0900

    add get and delete post

--- routes/api/post.js
+++ routes/api/post.js
@@ -5,7 +5,7 @@ const passport = require('passport');
 
 // model
 const postModel = require('../../models/post');
-
+const profileModel = require('../../models/pofile');
 // validation
 const validatePostInput = require('../../validation/post');
 
@@ -22,10 +22,36 @@ router.get('/test', (req, res) => {
     });
 });
 
+// @route GET api/post/all
+// @desc get all post
+// @access publie
+router.get('/all', (req, res) => {
+    postModel
+        .find()
+        .sort({ date: -1 })
+        .then(posts => {
+            res.json(posts)
+        })
+        .catch(err => res.json(err));
+});
+
+// @route GET api/post/:post_id
+// @desc get post by post_id
+// @access Public
+router.get('/:post_id', (req, res) => {
+
+    postModel
+        .findById(req.params.post_id)
+        .then(post => {
+            res.json(post);
+        })
+        .catch(err => res.json(err));
+
+});
+
 // @route POST api/post
 // @desc post register
 // @access Private
-
 router.post('/', authCheck, (req, res) => {
    const { errors, isValid } = validatePostInput(req.body);
 
@@ -47,6 +73,33 @@ router.post('/', authCheck, (req, res) => {
         })
         .catch(err => res.json(err));
 
+});
+
+// @ route DELTE api/post/:post_id
+// @ desc Delete post
+// @ access Private
+router.delete('/:post_id', authCheck, (req, res) => {
+    
+    profileModel
+        .findOne({ user: req.user.id })
+        .then(profile => {
+            postModel
+                .findById(req.params.post_id)
+                .then(post => {
+                    // check for post owner
+                    if (post.user.toString() !== req.user.id) {
+                        return res.status(401).json({
+                            msg: 'User not authorized'
+                        });
+                    } 
+                    post
+                        .remove()
+                        .then( () => res.json({ msg: 'Successful post Delete' }) )
+                        .catch(err => res.json(err));
+                })
+                .catch(err => res.json(err));
+        })
+        .catch(err => res.json(err));
 });
 
 module.exports = router;
