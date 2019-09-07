cc34e8a47170bc2181d71e6fc9249cb032c61026
park <ckdwls2525@naver.com>
2019-09-07 11:37:40 +0900

    add Profiles, Login, Register

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,32 @@ import React, { Component } from 'react';
 import Footer from './Components/layout/Footer';
 import Landing from './Components/layout/Landing';
 import Navbar from './Components/layout/Navbar';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Register from './Components/auth/Register';
+import Login from './Components/auth/Login';
+import Profiles from './Components/auth/Profiles';
 
 class App extends Component {
   
   render() {
     return (
-      <div>
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      // <div>
+      //   <Navbar />
+      //   <Landing />
+      //   <Footer />
+      // </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <div className="container">
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/profiles' component={Profiles} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
---
+++ client/src/Components/auth/Profiles.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Profiles extends Component {
+    render() {
+
+        return (
+            <div>
+                <h1>Profiles</h1>
+            </div>
+        );
+
+    }
+}
+
+export default Profiles;
