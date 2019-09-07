f169b0b8c4b31d597b8af130c0bbd22454647372
park <ckdwls2525@naver.com>
2019-09-07 12:09:43 +0900

    Login Form

--- client/src/Components/auth/Login.js
+++ client/src/Components/auth/Login.js
@@ -1,11 +1,72 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            password: '',
+            errors: {}
+        };
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        
+        const newUser = {
+            email: this.state.email,
+            password: this.state.password
+        };
+
+        console.log(newUser);
+
+    }
+
     render() {
 
         return (
-            <div>
-                <h1>Login</h1>
+            <div className="register">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-8 m-auto">
+                            <h1 className="display-4 text-center">Login</h1>
+                            <p className="lead text-center">
+                                Write your Id & Password
+                            </p>
+                            <form onSubmit={this.onSubmit}>
+                                <div className="form-group">
+                                    <input
+                                        type="email"
+                                        className="form-control form-control-lg"
+                                        placeholder="insert your email"
+                                        name="email"
+                                        value={this.state.email}
+                                        onChange={this.onChange}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <input
+                                        type="password"
+                                        className="form-control form-control-lg"
+                                        placeholder="insert your password"
+                                        name="password"
+                                        value={this.state.password}
+                                        onChange={this.onChange}
+                                    />
+                                </div>
+                                <input type="submit" className="btn btn-info btn-block mt-4" value="Login" value="SignUp"/>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
 
